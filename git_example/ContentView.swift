b84eb44f9178b09d87aacef27eca6e235ad78957
Annie Wang <s028016@students.lmsd.org>
2021-03-11 12:11:20 -0500

    all files are now tracked

---
+++ git_example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git_example
+//
+//  Created by Annie Wang (student LM) on 3/11/21.
+//  Copyright © 2021 Annie Wang (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
